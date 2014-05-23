e4a21056d79e8bb8ff09950d7adf9ef1996a10a9
Ian Tester <imroykun@gmail.com>
2014-05-23 12:28:58 +1000

    New circle drawing code using scan-line techniques.
    Slower, but I hope to have anti-aliased rendering eventually.

--- Surface-draw.cc
+++ Surface-draw.cc
@@ -16,78 +16,117 @@
         You should have received a copy of the GNU General Public License
         along with NavSpark tools.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <iostream>
+#include <map>
+#include <memory>
+#include <math.h>
 #include "Surface-draw.hh"
 
 namespace GPSstatus {
 
-  void draw_circle(SDL_Surface *surface, double cx, double cy, double radius, SDL_Colour colour) {
-    double i = 0, j = radius;
-    double e = 1 - radius;
-    unsigned int col = SDL_MapRGBA(surface->format, colour.r, colour.g, colour.b, colour.a);
-    while (1) {
-      int x1 = floor(cx - i);
-      int x2 = floor(cx + i);
-      int y1 = floor(cy - j);
-      int y2 = floor(cy + j);
+  struct Intersections {
+    int start, end;
+    std::map<int, std::pair<double, double> > intersections;
 
-      draw_pixel(surface, x1, y1, col);
-      draw_pixel(surface, x2, y1, col);
-      draw_pixel(surface, x1, y2, col);
-      draw_pixel(surface, x2, y2, col);
+    inline Intersections(int s, int e) :
+      start(s), end(e)
+    {}
 
-      int y3 = floor(cy - i);
-      if (y1 == y3)
-	break;
-      int y4 = floor(cy + i);
-      if (y2 == y4)
-	break;
-      int x3 = floor(cx - j);
-      int x4 = floor(cx + j);
+    inline std::pair<double, double>& operator [](int i) { return intersections[i]; }
+    inline bool has(int i) { return intersections.count(i) > 0; }
 
-      draw_pixel(surface, x3, y3, col);
-      draw_pixel(surface, x4, y3, col);
-      draw_pixel(surface, x3, y4, col);
-      draw_pixel(surface, x4, y4, col);
+    typedef std::shared_ptr<Intersections> ptr;
+  }; // struct Intersections
 
-      i++;
-      if (e < 0) {
-	e += 2 * i + 1;
-      } else {
-	j--;
-	e += 2 * (i - j + 1);
+  template <typename T>
+  inline T sqr(T x) { return x * x; }
+
+  Intersections::ptr circle_intersections(double cx, double cy, double radius) {
+    int miny = floor(1 + cy - radius);
+    int maxy = floor(cy + radius);
+    auto ins = std::make_shared<Intersections>(miny, maxy);
+
+    double rsq = radius * radius;
+    for (int y = miny; y <= maxy; y++) {
+      double d = rsq - sqr(y - cy);
+      if (d >= 0.0) {
+	double dsqrt = sqrt(d);
+	(*ins)[y] = std::make_pair(cx - dsqrt, cx + dsqrt);
       }
     }
+
+    return ins;
+  }
+
+  void draw_circle(SDL_Surface *surface, double cx, double cy, double radius, SDL_Colour colour) {
+    unsigned int col = SDL_MapRGBA(surface->format, colour.r, colour.g, colour.b, colour.a);
+
+    auto outer = circle_intersections(cx, cy, radius + 0.5);
+    auto inner = circle_intersections(cx, cy, radius - 0.5);
+
+    int miny = outer->start;
+    if (miny < 0)
+      miny = 0;
+    int maxy = outer->end;
+    if (maxy >= surface->h)
+      maxy = surface->h - 1;
+
+    for (int y = miny; y <= maxy; y++) {
+      int x1 = floor((*outer)[y].first);
+      int x4 = floor(0.5 + (*outer)[y].second);
+
+      if (inner->has(y)) {
+	int x2 = floor(0.5 + (*inner)[y].first);
+	int x3 = floor((*inner)[y].second);
+	if ((x2 >= 0) && (x1 < surface->w)){
+	  if (x1 < 0)
+	    x1 = 0;
+	  if (x2 >= surface->w)
+	    x2 = surface->w - 1;
+	  draw_hline(surface, x1, x2, y, col);
+	}
+
+	if ((x4 >= 0) && (x3 < surface->w)){
+	  if (x3 < 0)
+	    x3 = 0;
+	  if (x4 >= surface->w)
+	    x4 = surface->w - 1;
+	  draw_hline(surface, x3, x4, y, col);
+	}
+      } else {
+	if ((x4 >= 0) && (x1 < surface->w)){
+	  if (x1 < 0)
+	    x1 = 0;
+	  if (x4 >= surface->w)
+	    x4 = surface->w - 1;
+	  draw_hline(surface, x1, x4, y, col);
+	}
+      }
+
+    }
+
   }
 
   void draw_filled_circle(SDL_Surface *surface, double cx, double cy, double radius, SDL_Colour colour) {
-    double i = 0, j = radius;
-    double e = 1 - radius;
     unsigned int col = SDL_MapRGBA(surface->format, colour.r, colour.g, colour.b, colour.a);
-    while (1) {
-      int x1 = floor(cx - i);
-      int x2 = floor(cx + i);
-      int y1 = floor(cy - j);
-      int y2 = floor(cy + j);
-      draw_hline(surface, x1, x2, y1, col);
-      draw_hline(surface, x1, x2, y2, col);
+    auto outline = circle_intersections(cx, cy, radius);
 
-      int y3 = floor(cy - i);
-      if (y1 == y3)
-	break;
-      int y4 = floor(cy + i);
-      if (y2 == y4)
-	break;
-      int x3 = floor(cx - j);
-      int x4 = floor(cx + j);
-      draw_hline(surface, x3, x4, y3, col);
-      draw_hline(surface, x3, x4, y4, col);
+    int miny = outline->start;
+    if (miny < 0)
+      miny = 0;
+    int maxy = outline->end;
+    if (maxy >= surface->h)
+      maxy = surface->h - 1;
 
-      i++;
-      if (e < 0) {
-	e += 2 * i + 1;
-      } else {
-	j--;
-	e += 2 * (i - j + 1);
+    for (int y = miny; y <= maxy; y++) {
+      int x1 = floor((*outline)[y].first);
+      int x2 = floor((*outline)[y].second - 0.5);
+      if ((x2 >= 0) && (x1 < surface->w)){
+	if (x1 < 0)
+	  x1 = 0;
+	if (x2 >= surface->w)
+	  x2 = surface->w - 1;
+	draw_hline(surface, x1, x2, y, col);
       }
     }
   }
