5c8db4dd1b9f0c63a257dccf46d897e9aa896457
Ian Tester <imroykun@gmail.com>
2014-05-28 17:09:18 +1000

    Simple program for parsing the binary stream from a SkyTraq receiver.

---
+++ bin.cc
@@ -0,0 +1,36 @@
+/*
+        Copyright 2014 Ian Tester
+
+        This file is part of NavSpark tools.
+
+        NavSpark tools is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        NavSpark tools is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with NavSpark tools.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include "SkyTraqBin.hh"
+
+int main(int argc, char* argv[]) {
+  unsigned char buffer[16];
+  while (1) {
+    std::cin.read((char*)buffer, 16);
+    try {
+      std::vector<SkyTraqBin::Output_message::ptr> messages = SkyTraqBin::parse_messages(buffer, std::cin.gcount());
+    } catch (std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+
+  return 0;
+}
